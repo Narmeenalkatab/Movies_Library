0cffd4d81ac44e29155daac291a44c428b98d604
Narmeen alkatab <narmeen.kattab1@gmail.com>
2023-03-30 21:06:11 +0000

    add the dot

--- schema.sql
+++ schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE moviesTable (
      id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     overview VARCHAR(255),
-    imag VARCHAR(255)
+    imag VARCHAR(255),
     comments VARCHAR(255)
 );
